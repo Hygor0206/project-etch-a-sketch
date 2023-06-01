cd2c82b60929ac6a4fb2b37f12c1ad0b583201da
Albert <jxjfifa@gmail.com>
2023-06-01 00:36:23 -0300

    Color picker add and fix height and width

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const container = document.getElementById("container");
-const containerSize=document.getElementById("containerSize");
+const gridContainer = document.getElementById("grid");
+const slider=document.getElementById("sizeSlider");
+const sliderValue = document.getElementById("sizeValue");
+const inputColor = document.getElementById("colorPicker");
+const colorMode = document.getElementById("colorBtn");
+const rainbow = document.getElementById("rainbowBtn");
 
 let mouseDown = false;
-let color = "black";
+let color="#000000";
 
 // Create dynamically grid items based in the slide bar size
 function makeRows(size) {
-  container.innerHTML = '';
-  container.style.setProperty('--grid-size', size);
-  container.style.setProperty('--grid-size', size);
+  gridContainer.innerHTML = '';
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
   for (i = 0; i < (size**2); i++) {
     let cell = document.createElement("div");
 
@@ -25,20 +29,27 @@ function makeRows(size) {
     // A event when the mouse over a grid item and them change his color
     cell.addEventListener("mouseover",() =>{
       if(mouseDown){
-        cell.style.backgroundColor = color;
+        cell.style.backgroundColor = color; // Color change according to color input
       }
     });
 
-    container.appendChild(cell).className = "grid-item";
+    gridContainer.appendChild(cell).className = "grid-item";
   };
 };
 
 let size=16;
 makeRows(size);
 
-containerSize.addEventListener('input', () => {
-    size=document.getElementById('containerSize').value;
+// Slider Bar to change the size of grid
+slider.addEventListener('input', () => {
+    size=document.getElementById('sizeSlider').value;
+    sliderValue.textContent = size+"x"+size;
     makeRows(size);
+});
+
+// Change the brush color
+inputColor.addEventListener("change", () => {
+  color=inputColor.value; 
 });
 
 // oninput="rangeValue.innerText = this.value"

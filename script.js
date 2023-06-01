e61d1dc6e8247407661696c4e8414b8f084dfb9f
Albert <jxjfifa@gmail.com>
2023-05-31 23:28:27 -0300

    Add a slider bar to change the grid size and add the funcstions to paint the grid item with mouse.

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 const container = document.getElementById("container");
-let containerSize=document.getElementById("containerSize");
+const containerSize=document.getElementById("containerSize");
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (i = 0; i < (rows * cols); i++) {
+let mouseDown = false;
+let color = "black";
+
+// Create dynamically grid items based in the slide bar size
+function makeRows(size) {
+  container.innerHTML = '';
+  container.style.setProperty('--grid-size', size);
+  container.style.setProperty('--grid-size', size);
+  for (i = 0; i < (size**2); i++) {
     let cell = document.createElement("div");
+
+    // A event when the mouse button is clicked
+    cell.addEventListener("mousedown",() =>{
+        mouseDown=true;
+    });
+
+    // A event when the mouse button was released
+    cell.addEventListener("mouseup",() =>{
+        mouseDown=false;
+    });
+    
+    // A event when the mouse over a grid item and them change his color
+    cell.addEventListener("mouseover",() =>{
+      if(mouseDown){
+        cell.style.backgroundColor = color;
+      }
+    });
+
     container.appendChild(cell).className = "grid-item";
   };
 };
 
 let size=16;
+makeRows(size);
 
 containerSize.addEventListener('input', () => {
     size=document.getElementById('containerSize').value;
-    console.log(size);
-    makeRows(size, size);
+    makeRows(size);
 });
-
-makeRows(size, size);
 
 // oninput="rangeValue.innerText = this.value"
